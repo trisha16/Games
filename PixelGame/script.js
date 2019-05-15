19f89840e5b95c153570a1661d0ba83e09292c2d
trisha16 <47844255+trisha16@users.noreply.github.com>
2019-05-15 09:56:55 +0530

    Add files via upload

---
+++ PixelGame/script.js
@@ -0,0 +1,9 @@
+function setpixcelcolor(pixcel)
+{
+  pixcel.style.backgroundColor = pencolor
+}
+var pencolor = "#DE1DF9";   //set default pen color
+function setpencolor(pen)
+{
+  pencolor=pen;
+}
